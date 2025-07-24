377193feafd93287979c5d7836b85f9600f71d98
rmarti55 <rmarti55@users.noreply.github.com>
2025-07-23 18:49:10 -0600

    Initial setup: React + TypeScript + Vite + Storybook + Chakra UI with atomic design structure

---
+++ .storybook/preview.ts
@@ -0,0 +1,23 @@
+import type { Preview } from '@storybook/react-vite'
+import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <ChakraProvider>
+        <Story />
+      </ChakraProvider>
+    ),
+  ],
+};
+
+export default preview;
