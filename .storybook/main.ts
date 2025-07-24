ba94eecd38e719f29e82ff2f030516a8e580a272
rmarti55 <rmarti55@users.noreply.github.com>
2025-07-23 19:04:51 -0600

    Remove all addons and disable auto-documentation to eliminate Chakra UI components from Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,15 +4,15 @@ const config: StorybookConfig = {
   "stories": [
     "../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-docs"
-  ],
+  "addons": [],
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
   },
   "typescript": {
     "check": false
-  }
+  },
+
+
 };
 export default config;
