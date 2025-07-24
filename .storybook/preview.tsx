549507a0d437d43aceb1515da0df76ff56f9560c
rmarti55 <rmarti55@users.noreply.github.com>
2025-07-23 18:51:23 -0600

    Fix ChakraProvider TypeScript errors for build

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,4 @@
 import type { Preview } from '@storybook/react-vite'
-import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 
 const preview: Preview = {
@@ -11,13 +10,6 @@ const preview: Preview = {
       },
     },
   },
-  decorators: [
-    (Story) => (
-      <ChakraProvider>
-        <Story />
-      </ChakraProvider>
-    ),
-  ],
 };
 
 export default preview; 
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import { Box, Heading, Text } from '@chakra-ui/react'
 
 function App() {
   return (
-    <ChakraProvider>
-      <Box p={8}>
-        <Heading size="lg" mb={4}>
-          e.l.f. Components
-        </Heading>
-        <Text>
-          This is the main application. All components will be developed and previewed in Storybook.
-        </Text>
-      </Box>
-    </ChakraProvider>
+    <Box p={8}>
+      <Heading size="lg" mb={4}>
+        e.l.f. Components
+      </Heading>
+      <Text>
+        This is the main application. All components will be developed and previewed in Storybook.
+      </Text>
+    </Box>
   )
 }
 
