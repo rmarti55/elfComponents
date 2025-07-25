0b17b4b3a4a8a8dc42e209c63a1e65ceb06af2d9
rmarti55 <rmarti55@users.noreply.github.com>
2025-07-24 18:35:02 -0600

    feat: update Storybook global configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
@@ -11,6 +12,17 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ 
+        padding: '0 16px',
+        margin: 0,
+        boxSizing: 'border-box',
+      }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default preview;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,15 +1,28 @@
-import type { Preview } from '@storybook/react-vite'
-import React from 'react'
+import type { Preview } from "@storybook/react";
+import React from 'react';
 
 const preview: Preview = {
   parameters: {
+    layout: 'fullscreen',
+    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  decorators: [
+    (Story) => (
+      <div style={{ 
+        padding: '0 16px',
+        margin: 0,
+        boxSizing: 'border-box',
+      }}>
+        <Story />
+      </div>
+    ),
+  ],
 };
 
-export default preview; 
+export default preview;
